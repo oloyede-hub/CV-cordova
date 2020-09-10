5acbf666285d959e2f75ad00f32ea138ee5a73a8
oloyede sodiq <sodiqoloyede295@gmail.com>
2020-09-10 16:08:46 +0100

    integrated Appjs into my cordova app and also changed my background-color of #070708; and  used App js default style for the background-color

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,19 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+App.controller('home', function (page) {
+    // put stuff here
+  });
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+  App.controller('about', function (page) {
+    // put stuff here
+  });
+  App.controller('skill', function (page) {
+    // put stuff here
+  });
+  App.controller('experience', function (page) {
+    // put stuff here
+  });
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
+  try {
+    App.restore();
+  } catch (err) {
+    App.load('home');
+  }
